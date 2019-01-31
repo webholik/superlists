696e193604a9271d6ec4bda8844e770d0ea0c49c
Ankit <ankit28595@gmail.com>
2019-01-31 15:36:58 +0530

    Functional test now checks input to the to-do list

--- funtional_tests.py
+++ funtional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -15,18 +16,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # The page title mentions to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish the test")
-        # self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types "Kill all Mormons" in the text box
+        inputbox.send_keys("Kill all Mormons")
 
         # When she hits enter, the page updates, and now lists
         # "1: Kill all Mormons" as item in the to-do list
+        inputbox.send_keys(Keys.ENTER)
+        self.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Kill all Mormons' for row in rows)
+        )
 
         # There is a still a text box inviting her to add another item
         # She enters "Kill all Mormons pretenders"
+        self.fail("Finish the tests")
 
         # The page updates again and now shows two items in the list
 
