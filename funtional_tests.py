dca98eb50c7f9dffae1276361fc3af4c57a8e072
Ankit <ankit28595@gmail.com>
2019-02-01 14:06:48 +0530

    Added helper function for test

--- funtional_tests.py
+++ funtional_tests.py
@@ -8,6 +8,12 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
+    def check_list_in_table(self, text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(text, [row for row in rows])
+
+
     def tearDown(self):
         self.browser.quit()
 
@@ -35,15 +41,23 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("And all the rest")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
         # self.assertTrue(
         #     any(row.text == '1: Kill all Mormons' for row in rows),
         #     f"New to-do item did not appear in the table.\
         #     Contents were:\n{table.text}"
         # )
 
-        self.assertIn('1: Kill all Mormons', [row.text for row in rows])
+        # self.assertIn('1: Kill all Mormons', [row.text for row in rows])
+
+        self.check_list_in_table('1: Kill all Mormons')
+        self.check_list_in_table('2: And all the rest')
 
         # There is a still a text box inviting her to add another item
         # She enters "Kill all Mormons pretenders"
