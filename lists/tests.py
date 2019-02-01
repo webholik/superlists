b78b51bcb4715109e131aa407272fb8d01bc6391
Ankit <ankit28595@gmail.com>
2019-02-01 17:08:13 +0530

    Added url for creating new list, removed redundant tests

--- lists/tests.py
+++ lists/tests.py
@@ -9,26 +9,26 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_next':'New item'})
+    # def test_can_save_a_POST_request(self):
+    #     response = self.client.post('/', data={'item_next':'New item'})
 
-        self.assertEqual(Item.objects.count(),1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'New item')
+    #     self.assertEqual(Item.objects.count(),1)
+    #     new_item = Item.objects.first()
+    #     self.assertEqual(new_item.text, 'New item')
 
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_next':'New item'})
-        self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'], '/lists/01/')
+    # def test_redirects_after_POST(self):
+    #     response = self.client.post('/', data={'item_next':'New item'})
+    #     self.assertEqual(response.status_code,302)
+    #     self.assertEqual(response['location'], '/lists/01/')
 
-        # self.assertIn('New item', response.content.decode())
-        # self.assertTemplateUsed(response,'home.html')
+    #     # self.assertIn('New item', response.content.decode())
+    #     # self.assertTemplateUsed(response,'home.html')
 
-    def test_only_save_items_when_necessary(self):
-        response = self.client.get('/')
-        self.assertEqual(Item.objects.count(),0,
-                         "Saving empty items for GET request"
-                         )
+    # def test_only_save_items_when_necessary(self):
+    #     response = self.client.get('/')
+    #     self.assertEqual(Item.objects.count(),0,
+    #                      "Saving empty items for GET request"
+    #                      )
 
 
 
@@ -66,3 +66,21 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_item.text, first_saved_item.text)
+
+
+class NewListTest(TestCase):
+
+    def test_can_save_POST_request(self):
+        self.client.post('/lists/new', data={
+            'item_next':'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={
+            'item_next':'A new list item'
+        })
+        # self.assertEqual(response.status_code, 302)
+        # self.assertEqual(response['location'],'lists/01/')
+        self.assertRedirects(response, '/lists/01/')
--- lists/views.py
+++ lists/views.py
@@ -3,11 +3,15 @@ from django.http import HttpResponse
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_next'])
-        return redirect('/lists/01/')
-    else:
-        return render(request, 'home.html')
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_next'])
+    #     return redirect('/lists/01/')
+    # else:
+    return render(request, 'home.html')
 
 def view_list(request):
     return render(request, 'list.html', {'items':Item.objects.all()})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_next'])
+    return redirect('/lists/01/')
