009bbb392d80c95edce6d6bc3633e5fc4a6ee4e7
Ankit <ankit28595@gmail.com>
2019-09-03 16:34:18 +0530

    Chapter 7 completed

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from lists.models import Item,List
+# from django.http import HttpResponse
+from lists.models import Item, List
+
 
 def home_page(request):
     # if request.method == 'POST':
@@ -9,17 +10,20 @@ def home_page(request):
     # else:
     return render(request, 'home.html')
 
+
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
-    return render(request, 'list.html', {'list':list_})
+    # items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', {'list': list_})
+
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_next'],list=list_)
+    Item.objects.create(text=request.POST['item_next'], list=list_)
     return redirect(f'/lists/{list_.id}/')
+
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_next'],list=list_)
+    Item.objects.create(text=request.POST['item_next'], list=list_)
     return redirect(f'/lists/{list_id}/')
