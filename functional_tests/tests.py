de529900b2c6eda85a7b3c2120c541ac1dea9d65
Ankit <ankit28595@gmail.com>
2019-09-03 17:13:20 +0530

    First steps of making functional test for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,7 +10,9 @@ MAX_WAIT = 1
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        options = webdriver.FirefoxOptions();
+        options.add_argument('--headless')
+        self.browser = webdriver.Firefox(options=options)
 
     def wait_and_check_list_in_table(self, text):
         start_time = time.time()
@@ -122,6 +124,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
 
         # Satisfied both users leave
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_and_check_list_in_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+    
 
 
 if __name__ == '__main__':
